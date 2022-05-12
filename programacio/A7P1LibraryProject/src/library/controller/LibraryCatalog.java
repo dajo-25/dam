bc02bf03cf7fded9683b2613cfe2066d6f3e738a
dajo-25 <DjBosch25@gmail.com>
2022-05-12 15:20:16 +0200

    cavse

--- programacio/A7P1LibraryProject/src/library/controller/LibraryCatalog.java
+++ programacio/A7P1LibraryProject/src/library/controller/LibraryCatalog.java
@@ -1,5 +1,12 @@
 package library.controller;
 
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+import library.models.Book;
+
 public class LibraryCatalog {
 
+	private ArrayList<TreeSet<Book>> catalog = new ArrayList<>();
+	
 }
