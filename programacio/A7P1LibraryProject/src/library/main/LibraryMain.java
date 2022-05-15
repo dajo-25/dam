e2fe478b0b37aa839a45498a1953e2418e551caa
dajo-25 <DjBosch25@gmail.com>
2022-05-15 17:25:43 +0200

    aven+os

--- programacio/A7P1LibraryProject/src/library/main/LibraryMain.java
+++ programacio/A7P1LibraryProject/src/library/main/LibraryMain.java
@@ -3,12 +3,15 @@ package library.main;
 import java.util.Scanner;
 
 import library.controller.LibraryCatalog;
+import library.models.Book;
 
 public class LibraryMain {
 
 	public static void main(String[] args) {
 		
 		LibraryCatalog libraryCatalog = new LibraryCatalog();
+		
+		libraryCatalog.loadFile("library.csv");
 		
 		Scanner scan = new Scanner(System.in);
 		int option = -1;
@@ -36,21 +39,98 @@ public class LibraryMain {
 
 			case 1:
 				
+				System.out.println("Introdueixi les dades del nou llibre:" 
+						+ "\nTítol:");
+				
+				String title = scan.nextLine();
+				
+				System.out.println("ISBN:");
+				String isbn = scan.nextLine();
+				
+				System.out.println("Autor:");
+				String author = scan.nextLine();
+				
+				System.out.println("Categoria:");
+				String cathegory = scan.nextLine();
+				
+				System.out.println("Editorial:");
+				String publisher = scan.nextLine();
+				
+				System.out.println("Descripció:");
+				String description = scan.nextLine();
+				
+				System.out.println("Enllaç imatge:");
+				String imageLink = scan.nextLine();
+				
+				System.out.println("Nombre de pàgines:");
+				int nPages = Integer.parseInt(scan.nextLine());
+				
+				Book newBook = new Book(title, isbn, author, cathegory, publisher, description, imageLink, nPages);
+				
+				libraryCatalog.addBook(newBook);
 				
 				break;
 				
 			case 2:
 				
+				System.out.println("Introdueixi l'ISBN:\n");
+				
+				String isbnQuery = scan.nextLine();
+				
+				Book bookQuery = libraryCatalog.findByISBN(isbnQuery);
+				
+				if (bookQuery != null) {
+					
+					System.out.println(bookQuery.toString());
+					
+				}else {
+					
+					System.out.println("No s'ha trobat.");
+					
+				}
 				
 				break;
 				
 			case 3:
 				
+				System.out.println("Introdueixi el títol:\n");
+				
+				String titleQuery = scan.nextLine();
+				
+				Book queryBook = libraryCatalog.findByTitle(titleQuery);
+				
+				if (queryBook != null) {
+					
+					System.out.println(queryBook.toString());
+					
+				}else {
+					
+					System.out.println("No s'ha trobat.");
+					
+				}
 				
 				break;
 				
 			case 4:
 				
+				System.out.println("Quina categoria vol consultar?");
+				
+				for (String string : libraryCatalog.getCathegories()) {
+					
+					System.out.println("  · " + string);
+					
+				}
+				
+				String cathegoryQuery = scan.nextLine();
+				
+				System.out.println("Els llibres de la categoria " + cathegoryQuery + "són:");
+				
+				for (Book book : libraryCatalog.getBooksByCathegory(cathegoryQuery)) {
+					
+					System.out.println("  · " + book.toString());
+					
+				}
+
 				
 				break;
 				
